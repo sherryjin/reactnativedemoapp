66d1948bac638a8909e121f0becfc5f897bee627
Sherry Jin <sherry@afar.com>
2016-02-01 13:43:25 -0800

    working example

---
+++ app.js
@@ -0,0 +1,16 @@
+import React, { Component, View, Text } from 'react-native';
+
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+
+var App = React.createClass({
+  render() {
+    return (
+      <ScrollableTabView>
+        <ReactPage tabLabel="React" />
+        <FlowPage tabLabel="Flow" />
+        <JestPage tabLabel="Jest" />
+      </ScrollableTabView>
+    );
+  }
+});
+}
--- index.android.js
+++ index.android.js
@@ -1,51 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+var React = require('react-native');
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+var sf_lists = require('./sf_lists.json');
 
-class DemoProject extends Component {
-  render() {
+var DestinationPage = React.createClass({
+
+  getInitialState: function() {
+    return {
+      sf_lists: sf_lists, //replace with ajax network call for real data
+    };
+  },
+
+  render: function() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+      <React.View style={{position: "relative"}}>
+      <React.Image
+        source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} style={{width: 360, height: 200, position: "absolute"}} />
+      <React.Text style={{position: "absolute", color: "white", fontSize: 24, left: 150, top: 75, fontWeight: "bold"}}>Title</React.Text>
+      </React.View>
+    )
   },
 });
 
-AppRegistry.registerComponent('DemoProject', () => DemoProject);
+var MapsPage = React.createClass({
+  render: function() {
+    return (
+      <React.View >
+        <React.Text>
+            Map Page:
+        </React.Text>
+      </React.View>
+    )
+  },
+});
+
+var Demo = React.createClass({
+ render: function() {
+    return (
+          <ScrollableTabView>
+            <DestinationPage tabLabel="San Francisco" />
+            <MapsPage tabLabel="Map" />
+          </ScrollableTabView>
+        );
+  },
+});
+
+var styles = React.StyleSheet.create({
+ 
+    // For the container View
+    parent: {
+        padding: 16
+    },
+ 
+    // For the Text label
+    germanLabel: {
+        marginTop: 20,
+        fontWeight: 'bold'
+    },
+ 
+    // For the Text meaning
+    germanWord: {
+        marginTop: 15,
+        fontSize: 30,
+        fontStyle: 'italic'
+    }
+});
+
+React.AppRegistry.registerComponent('DemoProject', () => Demo);
