103566163b640145213ea85cfbf9b814bc251575
Sherry Jin <sherry@afar.com>
2016-02-01 15:38:37 -0800

    working list

--- index.android.js
+++ index.android.js
@@ -1,22 +1,56 @@
 var React = require('react-native');
 var ScrollableTabView = require('react-native-scrollable-tab-view');
-var sf_lists = require('./sf_lists.json');
+
+var dataSource = new React.ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
+var sf_lists = [
+  {
+    title: "12 Must Do Experiences In San Francisco",
+    image: "http://media.afar.com/uploads/images/post_images/images/u4LxFfuZ4C/large_open-uri20121009-8764-16cahet?1383797165"
+  },
+  {
+    title: "Chefs' Feed Picks: Where Chefs Eat in San Francisco",
+    image: "http://media.afar.com/uploads/images/post_images/images/5gJpCNxQ6S/large_be98cfbd9a82885ed46ee43e0d1970b1?1438159880"
+  },
+  {
+    title: "If You Only Have 3 Days In San Francisco",
+    image: "http://media.afar.com/uploads/images/post_images/images/newSLyGAjA/large_open-uri20130529-13039-16f4v7k?1383814579"
+  },
+  {
+    title: "San Francisco For Families",
+    image: "http://media.afar.com/uploads/images/post_images/images/FYARTo3nbX/large_open-uri20121203-1890-7a19bt?1383799902"
+  },
+  {
+    title: "San Francisco's Golden Gate Park",
+    image: "http://media.afar.com/uploads/images/post_images/images/nekJSuJMCX/large_7e5c33a89f5ef76b81c80d79c364f984?1438669641"
+  },
+  {
+    title: "The Best Hotels in San Francisco",
+    image: "http://media.afar.com/uploads/images/post_images/images/K3MTkQf7Js/large_3d60acd59c0dcd4340de023b73190a70?1438324236"
+  }
+]
 
 var DestinationPage = React.createClass({
 
   getInitialState: function() {
     return {
-      sf_lists: sf_lists, //replace with ajax network call for real data
+      dataSource: dataSource.cloneWithRows(sf_lists),
     };
+  },
+
+  renderItem: function(item) {
+    return (
+      <React.View style={{position: "relative", height: 200}}>
+        <React.Image source={{uri: item.image}} style={{width: 360, height: 200, position: "absolute"}} />
+        <React.View style={{position: "absolute", top: 75, marginLeft: 75, marginRight: 75}}>
+          <React.Text style={{position: "absolute", color: "white", fontSize: 16, fontWeight: "bold", textAlign: "center"}}>{item.title}</React.Text>
+        </React.View>
+      </React.View>
+    );
   },
 
   render: function() {
     return (
-      <React.View style={{position: "relative"}}>
-      <React.Image
-        source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} style={{width: 360, height: 200, position: "absolute"}} />
-      <React.Text style={{position: "absolute", color: "white", fontSize: 24, left: 150, top: 75, fontWeight: "bold"}}>Title</React.Text>
-      </React.View>
+      <React.ListView dataSource={this.state.dataSource} renderRow={this.renderItem}></React.ListView>
     )
   },
 });
