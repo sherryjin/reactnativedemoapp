1e541c2fe22690415a04bdfd62055605281be1aa
Sherry Jin <sherry@afar.com>
2016-02-02 18:01:27 -0800

    demo app working

--- index.android.js
+++ index.android.js
@@ -1,5 +1,6 @@
 var React = require('react-native');
 var ScrollableTabView = require('react-native-scrollable-tab-view');
+var MapView = require('react-native-maps');
 
 var dataSource = new React.ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
 var sf_lists = [
@@ -56,16 +57,69 @@ var DestinationPage = React.createClass({
 });
 
 var MapsPage = React.createClass({
+
+  getInitialState: function() {
+    return { 
+      detailView: false, 
+      region: { latitude: 37.78825, longitude: -122.4324, latitudeDelta: 0.0922, longitudeDelta: 0.0421},
+      markers: [
+        {coordinate: {latitude: 37.792537, longitude: -122.3933}, title: "Yank Sing", description: "Expensive dim sum", address: "1 Rincon Center", image_url: "http://cdn2.vox-cdn.com/uploads/chorus_asset/file/765230/yanksing_dimsumcart.0.jpg"}
+      ]
+    };
+  },
+
+  onRegionChange: function(region) {
+    this.setState({state: region });
+  },
+
+  pressedMarkerCallout: function(){
+    this.setState({detailView: true });
+  },
+
+  _onPressButton: function(){
+    this.setState({detailView: false });
+  },
+
   render: function() {
     return (
-      <React.View >
-        <React.Text>
-            Map Page:
-        </React.Text>
+      <React.View>
+        {(() => {
+            if (this.state.detailView) {
+             return (
+                <React.View>
+                  <React.Image source={{uri: this.state.markers[0].image_url}} style={{width: 360, height: 200}} />
+                  <React.Text style={{fontSize: 16, fontWeight: 'bold', textAlign: "left"}}>{this.state.markers[0].title}</React.Text>
+                  <React.Text>{this.state.markers[0].description}</React.Text>
+                  <React.Text>{this.state.markers[0].address}</React.Text>
+                  <React.Text style={{color: "black"}}>{"Sniff other cat's butt and hang jaw half open thereafter scream at teh bath. Chase mice chase mice plan steps for world domination. Inspect anything brought into the house intrigued by the shower, so sun bathe tuxedo cats always looking dapper. Claws in your leg sleep on keyboard bathe private parts with tongue then lick owner's face yet touch water with paw then recoil in horror. If it fits, i sits."}</React.Text>
+                  <React.TouchableNativeFeedback
+                      onPress={this._onPressButton}
+                      background={React.TouchableNativeFeedback.SelectableBackground()}>
+                      <React.View style={{width: 40, height: 30, backgroundColor: 'blue'}}>
+                      <React.Text style={{paddingLeft: 30, color: "white"}}>Back</React.Text>
+                    </React.View>
+                  </React.TouchableNativeFeedback>
+                </React.View>
+              );
+            } else {
+              return (
+                <MapView style={{width: 360, height: 360}} region={this.state.region} onRegionChange={this.onRegionChange}>
+                  {this.state.markers.map(marker => (
+                    <MapView.Marker key={marker.title}
+                      coordinate={marker.coordinate}
+                      title={marker.title}
+                      description={marker.description}
+                      onCalloutPress={this.pressedMarkerCallout}
+                    />
+                  ))}
+                </MapView>
+              );
+            }
+          })()}
       </React.View>
     )
   },
-});
+})
 
 var Demo = React.createClass({
  render: function() {
@@ -76,27 +130,6 @@ var Demo = React.createClass({
           </ScrollableTabView>
         );
   },
-});
-
-var styles = React.StyleSheet.create({
- 
-    // For the container View
-    parent: {
-        padding: 16
-    },
- 
-    // For the Text label
-    germanLabel: {
-        marginTop: 20,
-        fontWeight: 'bold'
-    },
- 
-    // For the Text meaning
-    germanWord: {
-        marginTop: 15,
-        fontSize: 30,
-        fontStyle: 'italic'
-    }
 });
 
 React.AppRegistry.registerComponent('DemoProject', () => Demo);
